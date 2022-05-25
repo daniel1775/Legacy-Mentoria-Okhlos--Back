b74a3a2cabd71b597135a9a0db711d06bab918c3
majoalba20 <mjalba458@gmail.com>
2022-05-25 12:17:28 -0500

    mentor and student user controller

--- src/controllers/UserMentorsController.js
+++ src/controllers/UserMentorsController.js
@@ -2,8 +2,8 @@ import db from '../db/db.js';
 
 export const getUserMentor = async (req,res) => {
     try {
-        const result = await db.query(`SELECT mentors.name, mentors.email, mentors.age, mentors.phone, studies.title, Businesses.name, Cargos.name FROM mentors, Businesses, studies, Cargos WHERE studies.id = mentors.id_studies and Businesses.id = mentors.id_bussiness and Cargos.id = mentors.id_cargo;`)
-        res.json(result);
+        const result = await db.query(`SELECT mentors.name, mentors.email, mentors.age, mentors.phone, studies.title, Businesses.name as company, Cargos.name as cargo FROM mentors, Businesses, studies, Cargos WHERE studies.id = mentors.id_studies and Businesses.id = mentors.id_bussiness and Cargos.id = mentors.id_cargo;`)
+        res.json(result[0]);
     } catch (error) {
         res.json({ message: error.message });
     }
@@ -11,9 +11,9 @@ export const getUserMentor = async (req,res) => {
 
 export const createUserStudent = async (req,res) => {
     try {
-        await db.query(`INSERT INTO users (email,password,role) VALUES ('${req.body.email}','test123','test');`)
+        await db.query(`INSERT INTO users (email,password,role) VALUES ('${req.body.email}','mentor123','mentor');`)
 
-        await db.query(`INSERT INTO Estudiantes (name,email,cohort,age,phone,status,gender, id_program,id_user) VALUES ('${req.body.name}','${req.body.email}',${req.body.cohort},${req.body.age},${req.body.phone},${req.body.status},'${req.body.gender}',(SELECT programs.id FROM programs WHERE programs.name = '${req.body.programa}'),(SELECT users.id FROM users WHERE users.email = '${req.body.email}'));`)
+        await db.query(`INSERT INTO mentors (name,email,age,sons,num_estudiantes,phone,status,gender,id_studies,id_bussiness, id_cargo,id_user) VALUES ('majoTest','${req.body.email}',56,3,2,23456,1,'Femenino',(SELECT studies.id FROM studies WHERE studies.title = 'Artes Visuales'),(SELECT Businesses.id FROM Businesses WHERE Businesses.name = 'Davivienda'),(SELECT Cargos.id FROM Cargos WHERE Cargos.name = 'Project Leader'),(SELECT users.id FROM users WHERE users.email = '${req.body.email}'));`)
 
 		res.json({
 			message: '¡Registro creado correctamente!',
