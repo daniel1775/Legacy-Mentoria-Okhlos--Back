bdf4dc1cae20858d76fd7a8a569b4dd4e67aa7e6
Trueqo <julianochoa1999@gmail.com>
2022-05-26 15:50:45 -0500

    route cohorte

--- src/db/db.js
+++ src/db/db.js
@@ -8,8 +8,8 @@ import { Sequelize } from "sequelize";
 
 // })
 
-const db = new Sequelize(process.env.DB_DATABASE || 'l5ppphiy12vci4m3', process.env.DB_USER || 'ovfdg77mq8lsi2mt', process.env.DB_PASSWORD || 'cb0mj6qzt0exkw0y',{
-    host: process.env.DB_HOST || 'ebh2y8tqym512wqs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+const db = new Sequelize(process.env.DB_DATABASE || 'heroku_e691640e5c518b5', process.env.DB_USER || 'ovfdg77mq8lsi2mt', process.env.DB_PASSWORD || 'a031feee',{
+    host: process.env.DB_HOST || 'us-cdbr-east-05.cleardb.net',
     dialect: 'mysql'
 })
 
