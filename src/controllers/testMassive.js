8eb5b9d40408881b1d8bb30b3e3a88eb8417aaa3
majoalba20 <mjalba458@gmail.com>
2022-05-26 14:55:55 -0500

    mentors and student email verification

--- src/controllers/testMassive.js
+++ src/controllers/testMassive.js
@@ -1,4 +1,5 @@
 import db from '../db/db.js';
+import UsersModel from '../models/UsersModel.js';
 
 export const getUserStudent = async (req,res) => {
     try {
@@ -12,12 +13,17 @@ export const getUserStudent = async (req,res) => {
 export const massiveDataStudent = async (req,res) => {
 
     let data = req.body
+
     try {
         for (let i = 0; i < data.length; i++) {
-            console.log(data[i])
-            await db.query(`INSERT INTO users (email,password,role) VALUES ('${data[i][1]}','student123','student');`)
+            const emailTest = await UsersModel.findOne({ where: { email: data[i][1]} });
+            if (emailTest) {
+                continue
+            } else {
+                await db.query(`INSERT INTO users (email,password,role) VALUES ('${data[i][1]}','student123','student');`)
 
-            await db.query(`INSERT INTO Estudiantes (name,email,cohort,age,phone,status,gender, id_program,id_user) VALUES ('${data[i][0]}','${data[i][1]}',${data[i][2]},${data[i][3]},${data[i][4]},${data[i][5]},'${data[i][6]}',(SELECT programs.id FROM programs WHERE programs.name = '${data[i][7]}'),(SELECT users.id FROM users WHERE users.email = '${data[i][1]}'));`)
+                await db.query(`INSERT INTO Estudiantes (name,email,cohort,age,phone,status,gender, id_program,id_user) VALUES ('${data[i][0]}','${data[i][1]}',${data[i][2]},${data[i][3]},${data[i][4]},${data[i][5]},'${data[i][6]}',(SELECT programs.id FROM programs WHERE programs.name = '${data[i][7]}'),(SELECT users.id FROM users WHERE users.email = '${data[i][1]}'));`)
+            } 
         }
         res.json({
             message: '¡Registro creado correctamente!',
