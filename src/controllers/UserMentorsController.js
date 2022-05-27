d3929a5da4332a0f795632c518fac1d8751d9dc7
majoalba20 <mjalba458@gmail.com>
2022-05-27 13:11:42 -0500

    mentor create massive

--- src/controllers/UserMentorsController.js
+++ src/controllers/UserMentorsController.js
@@ -6,11 +6,11 @@ export const getUserMentor = async (req, res) => {
       `SELECT mentors.name as nombreMentor, mentors.email, mentors.age, mentors.phone, studies.title, 
       businesses.name, cargos.name 
       FROM
-     mentors, businesses, studies, cargos 
-    WHERE
-     studies.id = mentors.id_studies and 
-    businesses.id = mentors.id_bussiness and
-     cargos.id = mentors.id_cargo;`
+      mentors, businesses, studies, cargos 
+      WHERE
+      studies.id = mentors.id_studies and 
+      businesses.id = mentors.id_bussiness and
+      cargos.id = mentors.id_cargo;`
     );
     res.json(result);
   } catch (error) {
@@ -18,31 +18,27 @@ export const getUserMentor = async (req, res) => {
   }
 };
 
-export const createUserStudent = async (req, res) => {
+export const createUserMentor = async (req,res) => {
+  let data = req.body
   try {
-    await db.query(
-      `INSERT INTO users (email,password,role) VALUES ('${req.body.email}','test123','test');`
-    );
+      for (let i = 0; i < data.length; i++) {
+          const emailTest = await UsersModel.findOne({ where: { email: data[i][1]} });
+          if (emailTest) {
+              continue
+          } else {
+              await db.query(`INSERT INTO users (email,password,role) VALUES ('${data[i][1]}','mentor123','mentor');`)
 
-    await db.query(
-      `INSERT INTO Estudiantes (name,email,cohort,age,phone,status,gender, id_program,id_user) VALUES ('${req.body.name}','${req.body.email}',${req.body.cohort},${req.body.age},${req.body.phone},${req.body.status},'${req.body.gender}',(SELECT programs.id FROM programs WHERE programs.name = '${req.body.programa}'),(SELECT users.id FROM users WHERE users.email = '${req.body.email}'));`
-    );
-
-    res.json({
-      message: "¡Registro creado correctamente!",
-    });
-  } catch (error) {
-    res.json({ message: error.message });
+              await db.query(`INSERT INTO mentors (name,email,age,sons,num_estudiantes,phone,status,gender,id_studies,id_bussiness, id_cargo,id_user) VALUES ('${data[i][0]}','${data[i][1]}',${data[i][2]},${data[i][3]},${data[i][4]},${data[i][5]},${data[i][6]},'${data[i][7]}',(SELECT studies.id FROM studies WHERE studies.title = '${data[i][8]}'),(SELECT Businesses.id FROM Businesses WHERE Businesses.name = '${data[i][9]}'),(SELECT Cargos.id FROM Cargos WHERE Cargos.name = '${data[i][10]}'),(SELECT users.id FROM users WHERE users.email = '${data[i][1]}'));`)
+          } 
+      }
+      res.json({
+          message: '¡Registro creado correctamente!',
+      });
+      res.end()
+  }catch (error) {
+      res.json({ message: error.message });
   }
-};
+}
 
-export const deleteAllUserStudent = async (req, res) => {
-  try {
-    await db.query(`DELETE FROM users WHERE users.role = 'student'; `);
-    res.json({
-      message: "¡Registro eliminado correctamente!",
-    });
-  } catch (error) {
-    res.json({ message: error.message });
-  }
-};
+
+
