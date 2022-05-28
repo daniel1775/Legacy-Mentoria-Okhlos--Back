ef59e92555c18a5eec3e0e1f48fc9ef68428dc87
majoalba20 <mjalba458@gmail.com>
2022-05-27 21:29:42 -0500

    db change heroku

--- src/db/db.js
+++ src/db/db.js
@@ -2,13 +2,12 @@ import { Sequelize } from "sequelize";
 
 
 
-/* const db = new Sequelize(process.env.DB_DATABASE || 'heroku_e691640e5c518b5', process.env.DB_USER || 'b1403ff4a9ad39', process.env.DB_PASSWORD || 'a031feee',{
+    const db = new Sequelize(process.env.DB_DATABASE || 'heroku_e691640e5c518b5', process.env.DB_USER || 'b1403ff4a9ad39', process.env.DB_PASSWORD || 'a031feee',{
     host: process.env.DB_HOST || 'us-cdbr-east-05.cleardb.net',
-    dialect: 'mysql', */
-    const db = new Sequelize(process.env.DB_DATABASE || 'okhlos', process.env.DB_USER || 'root', process.env.DB_PASSWORD || '',{
-        host: process.env.DB_HOST || 'localhost',
-        dialect: 'mysql',
-
+    dialect: 'mysql'
+    // const db = new Sequelize(process.env.DB_DATABASE || 'okhlos', process.env.DB_USER || 'root', process.env.DB_PASSWORD || '',{
+    //     host: process.env.DB_HOST || 'localhost',
+    //     dialect: 'mysql',
  })
 
 
