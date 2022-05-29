089af78d40d67a65d1ebdd10f44bad5fc3c600db
majoalba20 <mjalba458@gmail.com>
2022-05-29 17:41:14 -0500

    student and mentor revision

--- src/controllers/testMassive.js
+++ src/controllers/testMassive.js
@@ -23,6 +23,7 @@ export const massiveDataStudent = async (req,res) => {
                 await db.query(`INSERT INTO users (email,password,role) VALUES ('${data[i][1]}','student123','student');`)
 
                 await db.query(`INSERT INTO Estudiantes (name,email,cohort,age,phone,status,gender, id_program,id_user) VALUES ('${data[i][0]}','${data[i][1]}',${data[i][2]},${data[i][3]},${data[i][4]},${data[i][5]},'${data[i][6]}',(SELECT programs.id FROM programs WHERE programs.name = '${data[i][7]}'),(SELECT users.id FROM users WHERE users.email = '${data[i][1]}'));`)
+                //agregar intereses
             } 
         }
         res.json({
