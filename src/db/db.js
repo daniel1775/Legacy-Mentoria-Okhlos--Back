4611c0fae823d9ec1216ee85b31e3144aeaa278b
majoalba20 <mjalba458@gmail.com>
2022-05-29 18:37:51 -0500

    adding port db

--- src/db/db.js
+++ src/db/db.js
@@ -6,9 +6,10 @@ import { Sequelize } from "sequelize";
     // host: process.env.DB_HOST || 'us-cdbr-east-05.cleardb.net',
     // dialect: 'mysql'
 
-     const db = new Sequelize(process.env.DB_DATABASE || 'okhlos', process.env.DB_USER || 'root', process.env.DB_PASSWORD || '',{
+     const db = new Sequelize(process.env.DB_DATABASE || 'Okhlos', process.env.DB_USER || 'root', process.env.DB_PASSWORD || '',{
          host: process.env.DB_HOST || 'localhost',
          dialect: 'mysql',
+         port: 3307
  })
 
 
