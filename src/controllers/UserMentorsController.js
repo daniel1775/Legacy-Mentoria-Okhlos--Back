dc5743b16d077211c368f85287bfea9217cff9c3
majoalba20 <mjalba458@gmail.com>
2022-05-30 11:22:55 -0500

    adding interest to massive data (student and mentor)

--- src/controllers/UserMentorsController.js
+++ src/controllers/UserMentorsController.js
@@ -24,19 +24,27 @@ export const createUserMentor = async (req,res) => {
       for (let i = 0; i < data.length; i++) {
           const emailTest = await UsersModel.findOne({ where: { email: data[i][1]} });
           if (emailTest) {
-              continue
+            continue
           } else {
-              await db.query(`INSERT INTO users (email,password,role) VALUES ('${data[i][1]}','mentor123','mentor');`)
+            await db.query(`INSERT INTO users (email,password,role) VALUES ('${data[i][1]}','mentor123','mentor');`)
 
-              await db.query(`INSERT INTO mentors (name,email,age,sons,num_estudiantes,phone,status,gender,id_studies,id_bussiness, id_cargo,id_user) VALUES ('${data[i][0]}','${data[i][1]}',${data[i][2]},${data[i][3]},${data[i][4]},${data[i][5]},${data[i][6]},'${data[i][7]}',(SELECT studies.id FROM studies WHERE studies.title = '${data[i][8]}'),(SELECT Businesses.id FROM Businesses WHERE Businesses.name = '${data[i][9]}'),(SELECT Cargos.id FROM Cargos WHERE Cargos.name = '${data[i][10]}'),(SELECT users.id FROM users WHERE users.email = '${data[i][1]}'));`)
+            await db.query(`INSERT INTO mentors (name,email,age,sons,num_estudiantes,phone,status,gender,id_studies,id_bussiness, id_cargo,id_user) VALUES ('${data[i][0]}','${data[i][1]}',${data[i][2]},${data[i][3]},${data[i][4]},${data[i][5]},${data[i][6]},'${data[i][7]}',(SELECT studies.id FROM studies WHERE studies.title = '${data[i][8]}'),(SELECT Businesses.id FROM Businesses WHERE Businesses.name = '${data[i][9]}'),(SELECT Cargos.id FROM Cargos WHERE Cargos.name = '${data[i][10]}'),(SELECT users.id FROM users WHERE users.email = '${data[i][1]}'));`)
+
+            const id_mentor = await db.query(`SELECT mentors.id FROM mentors WHERE mentors.email = '${data[i][1]}';`)
+
+            //experiencia baja
+            await db.query(`INSERT INTO mentors_interests (mentors_interests.nivel, mentors_interests.id_mentor, mentors_interests.id_interest) VALUES (1,${id_mentor[0][0].id},(SELECT interests.id FROM interests WHERE interests.name = '${data[i][11]}'));`)
+
+            //experiencia alta
+            await db.query(`INSERT INTO mentors_interests (mentors_interests.nivel, mentors_interests.id_mentor, mentors_interests.id_interest) VALUES (2,${id_mentor[0][0].id},(SELECT interests.id FROM interests WHERE interests.name = '${data[i][12]}'));`)
           } 
       }
       res.json({
-          message: '¡Registro creado correctamente!',
+        message: '¡Registro creado correctamente!',
       });
       res.end()
   }catch (error) {
-      res.json({ message: error.message });
+    res.json({ message: error.message });
   }
 }
 
--- src/controllers/testMassive.js
+++ src/controllers/testMassive.js
@@ -3,7 +3,7 @@ import UsersModel from '../models/UsersModel.js';
 
 export const getUserStudent = async (req,res) => {
     try {
-        const result = await db.query(`SELECT Estudiantes.id, users.email, Estudiantes.name, Estudiantes.cohort, Estudiantes.age, Estudiantes.phone, Estudiantes.status, Estudiantes.gender, programs.name as programa FROM Estudiantes, users, programs WHERE Estudiantes.id_user = users.id and Estudiantes.id_program = programs.id ORDER BY Estudiantes.name;`)
+        const result = await db.query(`SELECT estudiantes.id,estudiantes.name,users.email, estudiantes.cohort, estudiantes.age, estudiantes.phone, estudiantes.status, estudiantes.gender, programs.name as programa FROM estudiantes, users, programs WHERE estudiantes.id_user = users.id and estudiantes.id_program = programs.id ORDER BY estudiantes.name;`)
         res.json(result[0]);
     } catch (error) {
         res.json({ message: error.message });
@@ -22,9 +22,18 @@ export const massiveDataStudent = async (req,res) => {
             } else {
                 await db.query(`INSERT INTO users (email,password,role) VALUES ('${data[i][1]}','student123','student');`)
 
-                await db.query(`INSERT INTO Estudiantes (name,email,cohort,age,phone,status,gender, id_program,id_user) VALUES ('${data[i][0]}','${data[i][1]}',${data[i][2]},${data[i][3]},${data[i][4]},${data[i][5]},'${data[i][6]}',(SELECT programs.id FROM programs WHERE programs.name = '${data[i][7]}'),(SELECT users.id FROM users WHERE users.email = '${data[i][1]}'));`)
-                //agregar intereses
-            } 
+                await db.query(`INSERT INTO estudiantes (name,email,cohort,age,phone,status,gender, id_program,id_user) VALUES ('${data[i][0]}','${data[i][1]}',${data[i][2]},${data[i][3]},${data[i][4]},${data[i][5]},'${data[i][6]}',(SELECT programs.id FROM programs WHERE programs.name = '${data[i][7]}'),(SELECT users.id FROM users WHERE users.email = '${data[i][1]}'));`)
+
+                const id_student = await db.query(`SELECT estudiantes.id FROM estudiantes WHERE estudiantes.email = '${data[i][1]}';`)
+
+                //interes bajo
+                await db.query(`INSERT INTO estudiante_interest (estudiante_interest.nivel, estudiante_interest.id_estudiante, estudiante_interest.id_interest) VALUES (1,${id_student[0][0].id},(SELECT interests.id FROM interests WHERE interests.name = '${data[i][8]}'));`)
+
+                //interes alto
+                await db.query(`INSERT INTO estudiante_interest (estudiante_interest.nivel, estudiante_interest.id_estudiante, estudiante_interest.id_interest) VALUES (2,${id_student[0][0].id},(SELECT interests.id FROM interests WHERE interests.name = '${data[i][9]}'));`)
+
+            }
+
         }
         res.json({
             message: '¡Registro creado correctamente!',
