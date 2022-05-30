618e7674a579f980b59c5d1a9a963f052c2bd91f
majoalba20 <mjalba458@gmail.com>
2022-05-30 13:11:25 -0500

    create student and update individual

--- src/controllers/StudentController.js
+++ src/controllers/StudentController.js
@@ -1,6 +1,7 @@
 import StudentModel from '../models/StudentModel.js';
 import db from '../db/db.js';
 import { Sequelize } from "sequelize";
+import UsersModel from '../models/UsersModel.js';
 const op = Sequelize.Op;
 
 
@@ -80,11 +81,27 @@ export const getOneStudent = async (req, res) => {
 
 //Este controlador siver para crear estudiantes
 export const createStudent = async (req, res) => {
+
+	let data = req.body
+
 	try {
-		await StudentModel.create(req.body);
+		const emailTest = await UsersModel.findOne({ where: { email: data[0][1]} });
+            if (!emailTest) {
+                await db.query(`INSERT INTO users (email,password,role) VALUES ('${data[0][1]}','student123','student');`)
+
+                await db.query(`INSERT INTO estudiantes (name,email,cohort,age,phone,status,gender, id_program,id_user) VALUES ('${data[0][0]}','${data[0][1]}',${data[0][2]},${data[0][3]},${data[0][4]},${data[0][5]},'${data[0][6]}',(SELECT programs.id FROM programs WHERE programs.name = '${data[0][7]}'),(SELECT users.id FROM users WHERE users.email = '${data[0][1]}'));`)
+
+                const id_student = await db.query(`SELECT estudiantes.id FROM estudiantes WHERE estudiantes.email = '${data[0][1]}';`)
+
+                //interes bajo
+                await db.query(`INSERT INTO estudiante_interest (estudiante_interest.nivel, estudiante_interest.id_estudiante, estudiante_interest.id_interest) VALUES (1,${id_student[0][0].id},(SELECT interests.id FROM interests WHERE interests.name = '${data[0][8]}'));`)
+
+                //interes alto
+                await db.query(`INSERT INTO estudiante_interest (estudiante_interest.nivel, estudiante_interest.id_estudiante, estudiante_interest.id_interest) VALUES (2,${id_student[0][0].id},(SELECT interests.id FROM interests WHERE interests.name = '${data[0][9]}'));`)
+            }
 		res.json({
 			message: '¡Registro creado correctamente!',
-		});
+		}); 
 	} catch (error) {
 		res.json({ message: error.message });
 	}
@@ -92,10 +109,24 @@ export const createStudent = async (req, res) => {
 
 //Este controlador sirve para actualizar estudiante
 export const updateStudent = async (req, res) => {
+
+	let data = req.body
+
 	try {
-		await StudentModel.update(req.body, {
-			where: { id: req.params.id },
-		});
+
+		const id_user = await db.query(`SELECT estudiantes.id_user FROM estudiantes WHERE estudiantes.id = ${req.params.id};`)
+
+		await db.query(`UPDATE users SET users.email = '${data[0][1]}' WHERE users.id = ${id_user[0][0].id_user};`)
+
+		await db.query(`UPDATE estudiantes SET estudiantes.name = '${data[0][0]}', estudiantes.email = '${data[0][1]}', estudiantes.cohort = ${data[0][2]}, estudiantes.age = ${data[0][3]}, estudiantes.phone = ${data[0][4]}, estudiantes.status = ${data[0][5]}, estudiantes.gender = '${data[0][6]}', estudiantes.id_program = ${data[0][7]}, estudiantes.id_user = ${id_user[0][0].id_user} WHERE estudiantes.id = ${req.params.id};`)
+
+		//interes bajo
+		await db.query(`UPDATE estudiante_interest SET estudiante_interest.nivel = 1, estudiante_interest.id_estudiante = ${req.params.id}, estudiante_interest.id_interest = ${data[0][8]} WHERE estudiante_interest.id_estudiante = ${req.params.id} and estudiante_interest.nivel = 1;`)
+
+		//interes alto
+		await db.query(`UPDATE estudiante_interest SET estudiante_interest.nivel = 2, estudiante_interest.id_estudiante = ${req.params.id}, estudiante_interest.id_interest = ${data[0][9]} WHERE estudiante_interest.id_estudiante = ${req.params.id} and estudiante_interest.nivel = 2;`)
+            
+
 		res.json({
 			message: '¡Registro actualizado correctamente!',
 		});
