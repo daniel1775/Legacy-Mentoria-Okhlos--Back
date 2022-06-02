b2d2d590bb52ac9ff60d4b2403a4a6e3698cbe6c
majoalba20 <mjalba458@gmail.com>
2022-06-02 13:06:12 -0500

    match almost done

--- src/controllers/MatchController.js
+++ src/controllers/MatchController.js
@@ -10,70 +10,10 @@ export const testMatch = async (req, res) => {
   const student_age = await getAgeStudent(id_student)
 
   let mentors_total = [];
+  let data = []
   // saved the 2 mentor's interests without add
   let mentorsInterestUnprocessed = [];
-  mentorsInterestUnprocessed.push(interests_low(mentors_interests,student_i_Low))
-  mentorsInterestUnprocessed.push(interests_high(mentors_interests,student_i_High))
-  mentorsInterestUnprocessed.push(age(mentors_interests,student_age))
-  
-  for (let i = 0; i < mentorsInterestUnprocessed[0].length; i++) {
-    mentors_total.push({
-      id: mentorsInterestUnprocessed[0][i].id_mentor,
-      total_score: mentorsInterestUnprocessed[0][i].interest_score_low + mentorsInterestUnprocessed[1][i].interest_score_high + mentorsInterestUnprocessed[2][i].age_score
-    })
-  }
-
-  let max_mentor = mentors_total[0];
-  for (let i = 1; i < mentors_total.length; i++) {
-    if(max_mentor.total_score < mentors_total[i].total_score){
-      max_mentor = mentors_total[i];
-    }
-  }
-
-  let porcentajeScoreIlow = 0
-  let porcentajeScoreIHigh = 0
-  let porcentajeScoreAge = 0
-
-  for (let i = 0; i < mentorsInterestUnprocessed[0].length; i++) {
-    if (mentorsInterestUnprocessed[0][i].id_mentor == max_mentor.id) {
-      porcentajeScoreIlow = (mentorsInterestUnprocessed[0][i].interest_score_low/25)*100
-      porcentajeScoreIHigh = (mentorsInterestUnprocessed[1][i].interest_score_high/25)*100
-      porcentajeScoreAge = (mentorsInterestUnprocessed[2][i].age_score/10)*100   
-      break 
-    }
-  }
-
-  let studenName = await db.query(`SELECT estudiantes.name FROM estudiantes WHERE estudiantes.id = ${id_student};`)
-
-  let mentorsName = await db.query(`SELECT mentors.name FROM mentors WHERE mentors.id = ${max_mentor.id};`)
-
-  let data = []
-
-  data.push({
-    mentorId: max_mentor.id,
-    mentorScore: (max_mentor.total_score/60)*100,
-    idStudent: id_student,
-    nameStudent: studenName[0][0].name,
-    nameMentor: mentorsName[0][0].name,
-    porcentajeScoreIlow: porcentajeScoreIlow,
-    porcentajeScoreIHigh: porcentajeScoreIHigh,
-    porcentajeScoreAge: porcentajeScoreAge
-  })
-  res.json(data[0])
-  res.end()
-};
-
-export const matchMassive = async (req,res)=>{
-  const students = req.body
-  let data = []
-  for (let i = 0; i < students.length; i++) {
-    const mentors_interests = await getMentorsMatch();
-    const student_i_Low = await getStudentInterestsLow(students[i])
-    const student_i_High = await getStudentInterestsHigh(students[i])
-    const student_age = await getAgeStudent(students[i])
-    let mentors_total = [];
-    // saved the 2 mentor's interests without add
-    let mentorsInterestUnprocessed = [];
+  if (mentors_interests.length != 0) {
     mentorsInterestUnprocessed.push(interests_low(mentors_interests,student_i_Low))
     mentorsInterestUnprocessed.push(interests_high(mentors_interests,student_i_High))
     mentorsInterestUnprocessed.push(age(mentors_interests,student_age))
@@ -105,30 +45,95 @@ export const matchMassive = async (req,res)=>{
       }
     }
 
-    let studenName = await db.query(`SELECT estudiantes.name FROM estudiantes WHERE estudiantes.id = ${students[i]};`)
+    let studenName = await db.query(`SELECT estudiantes.name FROM estudiantes WHERE estudiantes.id = ${id_student};`)
 
     let mentorsName = await db.query(`SELECT mentors.name FROM mentors WHERE mentors.id = ${max_mentor.id};`)
 
     data.push({
       mentorId: max_mentor.id,
       mentorScore: (max_mentor.total_score/60)*100,
-      idStudent: students[i],
+      idStudent: id_student,
       nameStudent: studenName[0][0].name,
       nameMentor: mentorsName[0][0].name,
       porcentajeScoreIlow: porcentajeScoreIlow,
       porcentajeScoreIHigh: porcentajeScoreIHigh,
       porcentajeScoreAge: porcentajeScoreAge
-    })    
-
-    // let num_students = await db.query(`SELECT mentors.num_estudiantes FROM mentors WHERE mentors.id = ${max_mentor.id};`)
-
-    // let num = num_students[0][0].num_estudiantes - 1
-    // console.log('num_estudiantes: ' + num)
-
-    // await db.query(`UPDATE mentors SET num_estudiantes = ${num} WHERE mentors.id = ${max_mentor.id};`)
-
+    })
+  }else{
+    data.push({message: "No hay mentor disponible!"})
   }
-  res.json(data)
+  res.json(data[0])
+  res.end()
+};
+
+export const matchMassive = async (req,res)=>{
+  const students = req.body
+  let data = []
+  for (let i = 0; i < students.length; i++) {
+    const mentors_interests = await getMentorsMatch();
+    if (mentors_interests.length != 0) {
+      const student_i_Low = await getStudentInterestsLow(students[i])
+      const student_i_High = await getStudentInterestsHigh(students[i])
+      const student_age = await getAgeStudent(students[i])
+      let mentors_total = [];
+      // saved the 2 mentor's interests without add
+      let mentorsInterestUnprocessed = [];
+      mentorsInterestUnprocessed.push(interests_low(mentors_interests,student_i_Low))
+      mentorsInterestUnprocessed.push(interests_high(mentors_interests,student_i_High))
+      mentorsInterestUnprocessed.push(age(mentors_interests,student_age))
+      
+      for (let i = 0; i < mentorsInterestUnprocessed[0].length; i++) {
+        mentors_total.push({
+          id: mentorsInterestUnprocessed[0][i].id_mentor,
+          total_score: mentorsInterestUnprocessed[0][i].interest_score_low + mentorsInterestUnprocessed[1][i].interest_score_high + mentorsInterestUnprocessed[2][i].age_score
+        })
+      }
+
+      let max_mentor = mentors_total[0];
+      for (let i = 1; i < mentors_total.length; i++) {
+        if(max_mentor.total_score < mentors_total[i].total_score){
+          max_mentor = mentors_total[i];
+        }
+      }
+
+      let porcentajeScoreIlow = 0
+      let porcentajeScoreIHigh = 0
+      let porcentajeScoreAge = 0
+
+      for (let i = 0; i < mentorsInterestUnprocessed[0].length; i++) {
+        if (mentorsInterestUnprocessed[0][i].id_mentor == max_mentor.id) {
+          porcentajeScoreIlow = (mentorsInterestUnprocessed[0][i].interest_score_low/25)*100
+          porcentajeScoreIHigh = (mentorsInterestUnprocessed[1][i].interest_score_high/25)*100
+          porcentajeScoreAge = (mentorsInterestUnprocessed[2][i].age_score/10)*100   
+          break 
+        }
+      }
+
+      let studenName = await db.query(`SELECT estudiantes.name FROM estudiantes WHERE estudiantes.id = ${students[i]};`)
+
+      let mentorsName = await db.query(`SELECT mentors.name FROM mentors WHERE mentors.id = ${max_mentor.id};`)
+
+      data.push({
+        mentorId: max_mentor.id,
+        mentorScore: (max_mentor.total_score/60)*100,
+        idStudent: students[i],
+        nameStudent: studenName[0][0].name,
+        nameMentor: mentorsName[0][0].name,
+        porcentajeScoreIlow: porcentajeScoreIlow,
+        porcentajeScoreIHigh: porcentajeScoreIHigh,
+        porcentajeScoreAge: porcentajeScoreAge
+      })    
+
+      let num_students = await db.query(`SELECT mentors.num_estudiantes FROM mentors WHERE mentors.id = ${max_mentor.id};`)
+
+      let num = num_students[0][0].num_estudiantes - 1
+
+      await db.query(`INSERT INTO matchs (score,cohort,id_mentor,id_estudiante, id_program) VALUES (${max_mentor.total_score},(SELECT estudiantes.cohort FROM estudiantes WHERE estudiantes.id = ${students[i]}),${max_mentor.id},${students[i]},(SELECT estudiantes.id_program FROM estudiantes where estudiantes.id = ${students[i]}));`)
+    }else{
+      data.push({message: "No hay mentor disponible!"})
+    }
+  }
+  res.json(data) 
   res.end()
 }
 
