19417936ca1222a759f9e0f363e51bd794c9707e
Trueqo <julianochoa1999@gmail.com>
2022-06-03 20:17:48 -0500

    .env for digitalOcean

--- src/db/db.js
+++ src/db/db.js
@@ -3,8 +3,8 @@ import { Sequelize } from "sequelize";
     // host: process.env.DB_HOST || 'us-cdbr-east-05.cleardb.net',
     // dialect: 'mysql'
 
-    const db = new Sequelize(process.env.DB_DATABASE || 'Okhlos', process.env.DB_USER || 'root', process.env.DB_PASSWORD || '',{
-        host: process.env.DB_HOST || 'localhost',
+    const db = new Sequelize(process.env.DB_DATABASE || 'l5ppphiy12vci4m3', process.env.DB_USER || 'ovfdg77mq8lsi2mt', process.env.DB_PASSWORD || 'cb0mj6qzt0exkw0y',{
+        host: process.env.DB_HOST || 'ebh2y8tqym512wqs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
         dialect: 'mysql',
         //port: process.env.port || 3307
 })
